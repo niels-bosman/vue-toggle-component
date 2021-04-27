afd75cf2fa594c1a0f3734775c519d740e10c4fc
nehlis <nielsbosman@mail.com>
2021-04-27 11:15:52 +0200

    Remove old data

--- src/install.js
+++ src/install.js
@@ -2,6 +2,7 @@ import VueToggle from "./components/VueToggle";
 
 export default {
     install(app) {
+        app.component("VueToggle", VueToggle);
         app.component("vue-toggle", VueToggle);
     }
 };
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,1 @@
-module.exports = {
-  chainWebpack: webpackConfig => {
-    webpackConfig.module.rule('vue').uses.delete('cache-loader')
-    webpackConfig.module.rule('js').uses.delete('cache-loader')
-    webpackConfig.module.rule('ts').uses.delete('cache-loader')
-  }
-}
+module.exports = {}
